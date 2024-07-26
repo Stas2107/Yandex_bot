91d07ae6b0ebbbbd39352370d41857ef7cf29b3f
Stas2107 <163865519+Stas2107@users.noreply.github.com>
2024-07-26 10:01:19 +0400

    ok

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import aiohttp
 import asyncio
-import logging
 from aiogram import Bot, Dispatcher, Router
 from aiogram.types import Message
 from aiogram.filters import Command
-from config import TOKEN, YANDEX_CAT, YANDEX_GPT
 import copy
+import os
+from dotenv import load_dotenv
 
-# Настройки логирования
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger(__name__)
+load_dotenv()
+
+TOKEN = os.getenv("TOKEN")
+YANDEX_CAT = os.getenv("YANDEX_CAT")
+YANDEX_GPT = os.getenv("YANDEX_GPT")
+
 
 # Настройки YandexGPT API
 url = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
@@ -19,8 +22,7 @@ headers = {
 }
 
 # Инициализация бота и диспетчера
-API_TOKEN = TOKEN
-bot = Bot(token=API_TOKEN)
+bot = Bot(token=TOKEN)
 dp = Dispatcher()
 router = Router()
 
@@ -48,26 +50,18 @@ async def get_gpt_response(user_message: str):
                 response.raise_for_status()
                 result = await response.json()
 
-                # Логируем полный ответ от API
-                logger.info(f"API response: {result}")
-
                 if 'result' in result and 'alternatives' in result['result']:
                     alternatives = result['result']['alternatives']
                     if alternatives:
                         return alternatives[0]['message']['text']
-                logger.warning("No valid choices in the API response.")
                 return "Извините, я не могу сейчас ответить."
     except aiohttp.ClientResponseError as errh:
-        logger.error(f"HTTP Error: {errh}")
         return f"HTTP Error: {errh}"
     except aiohttp.ClientConnectionError as errc:
-        logger.error(f"Connection Error: {errc}")
         return f"Connection Error: {errc}"
     except aiohttp.ClientTimeout as errt:
-        logger.error(f"Timeout Error: {errt}")
         return f"Timeout Error: {errt}"
     except aiohttp.ClientError as err:
-        logger.error(f"Something went wrong: {err}")
         return f"Something went wrong: {err}"
 
     # Обработчик команды /start
@@ -79,17 +73,13 @@ async def send_welcome(message: Message):
 @router.message()
 async def handle_message(message: Message):
     user_message = message.text
-    logger.info(f"Received message: {user_message}")
     response_text = await get_gpt_response(user_message)
     await message.answer(response_text)
 
-    # Основная функция для запуска бота
+# Основная функция для запуска бота
 async def main():
     dp.include_router(router)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    try:
-        asyncio.run(main())
-    except (KeyboardInterrupt, SystemExit):
-        logger.info("Bot stopped!")
+    asyncio.run(main())
